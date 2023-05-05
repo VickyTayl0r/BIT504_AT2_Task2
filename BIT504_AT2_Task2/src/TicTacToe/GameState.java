f7b8729914517a49574664d6b680602376b2d13a
Vicky Taylor <vicky_taylor62@hotmail.com>
2023-05-06 09:54:01 +1200

    Final Edit of code
    -Updated the layout of code.
    -Comments finalised.
    -Testing completed; code adjusted until program has no errors.

--- BIT504_AT2_Task2/src/TicTacToe/GameState.java
+++ BIT504_AT2_Task2/src/TicTacToe/GameState.java
@@ -1,8 +1,8 @@
 package TicTacToe;
-
+// The enum GameState defines the game states of this game
 public enum GameState {
-	Playing,
-	Draw,
-	Cross_won,
-	Nought_won
+		Playing,
+		Draw,
+		Cross_won,
+		Nought_won
 }
